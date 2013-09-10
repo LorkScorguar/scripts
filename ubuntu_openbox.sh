a82fcc58bc740f366ed6798b0ce28c8bc3317920
lorkscorguar@gmail.com <lorkscorguar@gmail.com>
2013-09-10 18:59:23 +0200

    Correcting install script

--- ubuntu_openbox.sh
+++ ubuntu_openbox.sh
@@ -15,8 +15,4 @@ sudo apt-get update
 sudo apt-get remove --purge onboard gnomine aisleriot bluez gnome-orca gnome-accessibility-themes gnome-sudoku thunderbird
 sudo apt-get install obmenu openbox openbox-theme lxappearance nitrogen tint2 xcompmgr gtk2-engines-nodoka vlc terminator guake chromium-browser virtualbox python3.2 espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp lo-menubar conky-all compizconfig-settings-manager flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors cowsay nmap 
 sudo apt-get dist-upgrade
-mv ../conkyrc ~/.conkyrc
-mv autostart ~/.config/openbox/autostart
-mv rc.xml ~/.config/openbox/rc.xml
-mv menu.xml ~/.config/openbox/menu.xml
 
