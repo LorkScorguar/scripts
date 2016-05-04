7e07158204d49f861de2df0d9d4277510156f7aa
Lork Scorguar <lorkscorguar@gmail.com>
2016-05-04 14:40:58 +0200

    adding go

--- ubuntu_work.sh
+++ ubuntu_work.sh
@@ -10,7 +10,7 @@ echo "Remove packages"
 sudo apt-get remove --purge onboard gnomine aisleriot bluez gnome-orca gnome-accessibility-themes gnome-sudoku thunderbird ppp pptp-linux network-manager-pptp network-manager-pptp-gnome bluez-alsa brltty telepathy-idle telepathy-logger telepathy-mission-control-5 telepathy-indicator telepathy-salut telepathy-gabble evolution-data-server
 sudo apt-get autoremove
 echo "Install softwares"
-sudo apt-get install vlc terminator chromium-browser virtualbox python3.5 espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot
+sudo apt-get install vlc terminator chromium-browser virtualbox python3.5 espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot golang golang-go
 echo "Upgrade system"
 sudo apt-get dist-upgrade
 echo "Installing Atom"
