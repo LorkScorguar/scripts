9d152effb955ff1ad8dfb3c276ae0e999d1ab1b4
Lork Scorguar <lorkscorguar@gmail.com>
2017-01-30 17:18:29 +0100

    use -y to avoid prompt

--- solus.sh
+++ solus.sh
@@ -1,10 +1,10 @@
 rm -rf Modèles Public
 #remove uneeded programs
-sudo eopkg remove thunderbird gnome-calculator bluez nvidia-304-glx-driver-modaliases nvidia-340-glx-driver-modaliases nvidia-glx-driver-modaliases orca xorg-driver-input-wacom -y
+sudo eopkg remove -y thunderbird gnome-calculator bluez nvidia-304-glx-driver-modaliases nvidia-340-glx-driver-modaliases nvidia-glx-driver-modaliases orca xorg-driver-input-wacom -y
 #install needed tools
-sudo eopkg install atom terminator libreoffice-writer libreoffice-calc git moc unrar mtpfs lm_sensors nmap golang -y
+sudo eopkg install -y atom terminator libreoffice-writer libreoffice-calc git moc unrar mtpfs lm_sensors nmap golang -y
 #upgrade
-sudo eopkg upgrade
+sudo eopkg upgrade -y
 #chrome dev
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-unstable/pspec.xml
 sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
