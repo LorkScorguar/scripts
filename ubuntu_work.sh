f7a7f46daa5a21340b27e000e964803c91ab26a4
Lork Scorguar <lorkscorguar@gmail.com>
2017-04-28 11:18:40 +0200

    Update ubuntu_work.sh

--- ubuntu_work.sh
+++ ubuntu_work.sh
@@ -10,7 +10,7 @@ echo "Remove packages"
 sudo apt-get remove --purge onboard gnomine aisleriot gnome-orca gnome-accessibility-themes gnome-sudoku thunderbird brltty telepathy-mission-control-5 evolution-data-server
 sudo apt-get autoremove
 echo "Install softwares"
-sudo apt-get install vlc terminator chromium-browser virtualbox espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot golang golang-go
+sudo apt-get install gconf2 gconf-service vlc terminator chromium-browser virtualbox espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot golang golang-go
 echo "Upgrade system"
 sudo apt-get dist-upgrade
 echo "Installing Atom"
