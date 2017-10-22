cafc7eeef8fde904823958d799353a2303dab724
Lork Scorguar <lorkscorguar@gmail.com>
2017-10-22 14:47:11 +0200

    Update ubuntu_work.sh

--- ubuntu_work.sh
+++ ubuntu_work.sh
@@ -10,7 +10,7 @@ echo "=======Remove packages======="
 sudo apt-get remove --purge -y onboard gnome-mines gnome-mahjongg xdiagnose unity-accessibility-profiles unity-scope-yelp checkbox-converged webbrowser-app xserver-xorg-input-wacom aisleriot gnome-orca gnome-accessibility-themes gnome-sudoku thunderbird rhythmbox deja-dup yelp brltty telepathy-mission-control-5 evolution-data-server
 sudo apt-get autoremove
 echo "=======Install softwares======="
-sudo apt-get install -y gconf2 gconf-service vlc terminator chromium-browser virtualbox espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg ffmpeg2theora gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good gstreamer0.10-ffmpeg build-essential gimp flashplugin-installer pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer0.10-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot golang golang-go
+sudo apt-get install -y vlc terminator chromium-browser virtualbox espeak mbrola mbrola-fr1 mbrola-fr4 ffmpeg gstreamer1.0-plugins-base gstreamer1.0-plugins-bad gstreamer1.00-plugins-ugly gstreamer1.0-plugins-good gstreamer1.0-ffmpeg build-essential gimp pocketsphinx-hmm-wsj1 pocketsphinx-lm-wsj python-pocketsphinx unrar moc gmtp moc-ffmpeg-plugin gstreamer1.0-fluendo-mp3 lm-sensors nmap git python-apt python-distutils-extra python-setuptools python-apt-dev python-dev libssl-dev libffi-dev libperl-dev jq android-tools-adb android-tools-fastboot golang golang-go
 echo "=======Upgrade system======="
 sudo apt-get dist-upgrade -y
 echo "=======Installing Atom======="
@@ -23,3 +23,4 @@ easy_install pip
 pip install -U pyopenssl ndg-httpsclient pyasnl
 pip install -U pip ansible
 sudo mkdir -p /etc/ansible
+sudo apt-get install -f
