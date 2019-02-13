68bef43bcd26319ce2df5cadd718531cc067d197
Lork Scorguar <lorkscorguar@gmail.com>
2019-02-13 16:25:59 +0100

    Update manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -1,6 +1,11 @@
-pacman -Syyu
-pamac remove xfburn thunderbird gcolor2 timeshift manjaro-hello clonezilla mintstick xterm manjaro-documentation-en
+rm -rf Modèles
+rm -rf Publics
+mkdir Dev
 cd Images && mkdir Wallpapers && cd Wallpapers && wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-744357.jpg && cd
 nitrogen --set-auto Images/Wallpapers/wallhaven-744357.jpg
+pacman -Syyu
+pamac remove xfburn thunderbird gcolor2 timeshift manjaro-hello clonezilla mintstick xterm manjaro-documentation-en
+#pamac install atom golang jq android-tools-adb android-tools-fastboot
 openbox --reconfigure
 openbox --restart
+sensors-detect
