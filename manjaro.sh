483e12dcbaf3e046f567a76e3e7f4d2ec071733b
Lork Scorguar <lorkscorguar@gmail.com>
2019-02-13 15:35:42 +0100

    Update manjaro.sh
    add a wallpaper

--- manjaro.sh
+++ manjaro.sh
@@ -1,2 +1,6 @@
 pacman -Syyu
-pamac remove xfburn thunderbird gcolor2 timeshift manjaro-hello
+pamac remove xfburn thunderbird gcolor2 timeshift manjaro-hello clonezilla mintstick xterm manjaro-documentation-en
+cd Images && mkdir Wallpapers && cd Wallpapers && wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-744357.jpg && cd
+nitrogen --set-auto Images/Wallpapers/wallhaven-744357.jpg
+openbox --reconfigure
+openbox --restart
