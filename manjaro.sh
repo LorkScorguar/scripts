3389e875a3cb70409cae73df2207b19831769020
Lork Scorguar <lorkscorguar@gmail.com>
2019-02-13 16:32:26 +0100

    Update manjaro.sh
    enable printing

--- manjaro.sh
+++ manjaro.sh
@@ -1,11 +1,19 @@
+#Home customizations
 rm -rf Modèles
 rm -rf Publics
 mkdir Dev
 cd Images && mkdir Wallpapers && cd Wallpapers && wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-744357.jpg && cd
 nitrogen --set-auto Images/Wallpapers/wallhaven-744357.jpg
+echo "alias ll='ls -l --color=auto'" > ~/.zshrc
+
+#Software installing/removing
 pacman -Syyu
 pamac remove xfburn thunderbird gcolor2 timeshift manjaro-hello clonezilla mintstick xterm manjaro-documentation-en
-#pamac install atom golang jq android-tools-adb android-tools-fastboot
+#pamac install atom golang jq android-tools-adb android-tools-fastboot manjaro-printer
+
+#System changes
 openbox --reconfigure
 openbox --restart
 sensors-detect
+sudo systemctl enable org.cups.cupsd.service
+sudo systemctl start org.cups.cupsd.service
